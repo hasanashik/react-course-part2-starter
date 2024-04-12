6c425c8396f7f588677a57461d060dc7849ce769
hasanashik <hasanashik100@gmail.com>
2024-04-12 18:21:53 +0600

    data mutation, form submit update data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
       <h1>React Starter Project</h1>
-      {/* <TodoList></TodoList> */}
-      <PostList />
+
+      {/* <PostList /> */}
+      <TodoForm />
+      <TodoList></TodoList>
     </>
   );
 }
